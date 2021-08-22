c4454430d2c5bca4d9cc88b434836f900478249b
Joaqo99 <joaqo9909@gmail.com>
2021-08-22 18:35:14 -0300

    Templates

--- finder/views.py
+++ finder/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import List, Guitar
 
 # Create your views here.
 def index(response):
-    return render(response, 'finder/index.html', {})
+    return render(response, "finder/index.html", {})
+
+def search(response):
+    return render(response, "finder/search.html", {})
+
+def lists(response, id):
+    g_list = List.objects.get(id=id)
+    return render(response, "finder/lists.html", {"name":g_list.name})
