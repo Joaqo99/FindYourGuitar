b6430f663a40d0bb0373cd9933c2394b6c913be5
Joaqo99 <joaqo9909@gmail.com>
2021-08-22 18:10:05 -0300

    Modelos 1

--- FindYourGuitar/settings.py
+++ FindYourGuitar/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'finder.apps.FinderConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'FindYourGuitar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\lucho\OneDrive\Escritorio\Programación\desarrollo web\FindYourGuitar\finder\templates\finder\index.html'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- finder/models.py
+++ finder/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class List(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+class Guitar(models.Model):
+    g_list = models.ForeignKey(List, on_delete=models.CASCADE, null=True)
+    brand = models.CharField(max_length=250, null=True)
+    g_model = models.CharField(max_length=250, null=True)
+    title = models.CharField(max_length=250, null=True)
+    description = models.TextField(max_length=2000, null=True)
+    link = models.CharField(max_length=1000, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+
+    def __str__(self):
+        return f'{self.brand} {self.g_model} --- ID: {self.id}'
