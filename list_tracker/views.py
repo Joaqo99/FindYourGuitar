fd3dc931fa4e0a76fd563bfd0d4d5ffc0336bbc0
Joaqo99 <joaqo9909@gmail.com>
2021-08-26 00:35:50 -0300

    Form create new list + empty list

--- list_tracker/views.py
+++ list_tracker/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 from .models import List
+from .forms import CreateNewList
 
 def mylists(response):
-    return render(response, "list_tracker/mylists.html", {"lists":List.objects.all()})
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            l = List(name=n)
+            l.save()
+            
+    form = CreateNewList()
+    my_dict = {"lists":List.objects.all(), "form":form}
+    return render(response, "list_tracker/mylists.html", my_dict)
