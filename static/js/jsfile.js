1ead13cfa443927020be005df6e95bc1d4bf36d9
Joaqo99 <joaqo9909@gmail.com>
2021-09-03 17:31:23 -0300

    styling de botones, falta hacer q dps del click se muestren

--- static/js/jsfile.js
+++ static/js/jsfile.js
@@ -1,5 +1,17 @@
-const arrows = document.querySelectorAll(".arrow")
+const arrows = document.querySelectorAll(".arrow");
+const menus = document.querySelectorAll(".buttons-menu");
+
+/* let components_list = []
 for (let arrow of arrows){
+    let components = [arrow]
+    components_list.push(components)
+}
+
+for (let menu of menus){
+    
+} */
+
+/* for (let arrow of arrows){
     let rotation = false
     arrow.addEventListener("click", ()=>{
         if (rotation == false){
@@ -8,6 +20,24 @@ for (let arrow of arrows){
         }
         else{
             arrow.style.transform = "rotate(0)";
+            rotation = false;
+        }
+    })
+}; */
+
+for (let ii in arrows){
+    let arrow = arrows[ii];
+    let menu = menus[ii];
+    let rotation = false;
+    arrow.addEventListener("click", ()=>{
+        if (rotation == false){
+            arrow.style.transform = "rotate(90deg)";
+            menu.style.display = "block";
+            rotation = true;
+        }
+        else{
+            arrow.style.transform = "rotate(0)";
+            menu.style.display = "none";
             rotation = false;
         }
     })
